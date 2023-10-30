fa83757f4ec35d6a38088915900ae201c46ed23c
Acemma78 <Acemma78@gmail.com>
2023-10-30 14:01:20 +0100

    payment code

--- class.py
+++ class.py
@@ -1,4 +1,23 @@
-def git(thing):
-    print(thing)
+hours = input("How many hours did you work this week?: ")
+base_pay = 12.5
 
-git("sexi")
+def git(hours, base_pay):
+    try:
+        hours = int(hours)
+    except:
+        print("input a valid amount")
+        quit()
+    if hours <= 40 and hours >= 0:
+         total_comp = (hours * base_pay)
+         print("total compensation is " + str(total_comp))
+    elif hours < 0:
+        print("no negatives")
+        quit()
+    else:
+        ovt = hours - 40
+        ovt_pay = ovt*1.5*base_pay
+        total_comp = (40*base_pay) + ovt_pay
+        print("total compensation is " + str(total_comp))
+
+git(hours, base_pay)
+    
