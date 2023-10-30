2d7cabb6d1b84088b9a3cdb672f09c630643f16e
Acemma78 <Acemma78@gmail.com>
2023-10-30 14:34:23 +0100

    added comments and edited variable and function names

--- class.py
+++ class.py
@@ -1,23 +1,28 @@
+# user input is collected stating the number of hours worked in a week
 hours = input("How many hours did you work this week?: ")
-base_pay = 12.5
+wage = 12.5
 
-def git(hours, base_pay):
+# the payment function takes hours worked and the wages per hour as its arguments
+def salary(hours, wage):
+# this checks if the input is a valid integer and throws a custom error if not
     try:
         hours = int(hours)
     except:
         print("input a valid amount")
         quit()
+#the following conditionals check if the hours worked are negative values, exactly 40 hours or above 40
     if hours <= 40 and hours >= 0:
-         total_comp = (hours * base_pay)
+         total_comp = (hours * wage)
          print("total compensation is " + str(total_comp))
     elif hours < 0:
         print("no negatives")
         quit()
     else:
+#this computes the overtime py based on increased pay rate of 1.5 times the normal rate
         ovt = hours - 40
-        ovt_pay = ovt*1.5*base_pay
-        total_comp = (40*base_pay) + ovt_pay
+        ovt_pay = ovt*1.5*wage
+        total_comp = (40*wage) + ovt_pay
         print("total compensation is " + str(total_comp))
 
-git(hours, base_pay)
+salary(hours, wage)
     
